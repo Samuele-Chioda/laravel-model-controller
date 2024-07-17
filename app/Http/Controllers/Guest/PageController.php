f9190fe511fff1f3664f2ff5553d0ad547a9b403
Samuele-Chioda <samuelechioda@gmail.com>
2024-07-18 01:28:22 +0200

    creazione db e Movie.php , errore

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -7,7 +7,5 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function index(){
 
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\PageController;
+use App\Http\Controllers\Guest\FilmController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [FilmController::class, 'index'])->name('guest.home');
